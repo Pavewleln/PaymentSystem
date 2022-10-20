227c8524d9aa004e394aa36249c302c675457f86
Pavel Kulikov <kulikovps2004@gmail.com>
2022-10-20 22:59:28 +0300

    fixed bugs historyLoader

--- src/App/Hoc/historyLoader.jsx
+++ src/App/Hoc/historyLoader.jsx
@@ -4,15 +4,10 @@ import {useEffect} from "react";
 import {getHistory, getHistoryLoadingStatus, loadHistoryList} from "../Store/history";
 
 export const HistoryLoader = ({children}) => {
-    const loading = useSelector(getHistoryLoadingStatus())
+    const isLoggedIn = useSelector(getIsLoggedIn());
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(loadHistoryList());
     }, []);
-    if(loading) return (
-        <div className="spinner-border text-dark" role="status">
-            <span className="sr-only"></span>
-        </div>
-    )
     return children;
 };
